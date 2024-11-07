f8e2f5b1b52dbd57e660ad2b154463f2e6bff956
KolbothOutdom <kolboth.o65@rsu.ac.th>
2024-11-07 23:52:18 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,87 +1,31 @@
-// Students write their code between these lines. 
-//
-// Code below this line vvvvv vvvvv vvvvv vvvvv vvvvv
-//
-// Code above this line ^^^^^ ^^^^^ ^^^^^ ^^^^^ ^^^^^
-//
-// Stuents should carefully read the test cases for the correct output. 
-//
+class NumberChecker {
+    constructor() { }
 
-// Mock Data 
-let balance = 5000;
-const transactionHistory = ['Deposited 10000 into Savings Account', 'Withdrawn 5000 from Savings Account'];
+    // Polymorphic function to check if numbers are odd or even
+    checkOddEven(input) {
+        if (typeof input === 'number') {
+        return input % 2 === 1 ? 'Odd':'Even'; 
+        }
+        else if (Array.isArray(input)){
+            return input.map(num => (typeof num === 'number' ? (num % 2 === 0 ? 'Even' : 'Odd') : ''));
+        }
+        else {
+            'Invalid input'
+        }
+    }
 
-function getBalance() {
-    // Write function description 
-
-    // Code below this line vvvvv vvvvv vvvvv vvvvv vvvvv
-    var getBalance = balance
-    return getBalance;
-
-
-    
-    // Code above this line ^^^^^ ^^^^^ ^^^^^ ^^^^^ ^^^^^
-}
-
-function deposit(amount) {
-    // Write function description 
-
-    // Code below this line vvvvv vvvvv vvvvv vvvvv vvvvv
-    balance += amount;
-    transactionHistory.push(`Deposited ${amount} into Savings Account`);
-    return amount;
-
-    
-    // Code above this line ^^^^^ ^^^^^ ^^^^^ ^^^^^ ^^^^^
-}
-
-function withdraw(amount) {
-    // Write function description 
-
-    // Code below this line vvvvv vvvvv vvvvv vvvvv vvvvv
-    balance -= amount;
-    transactionHistory.push(`Withdrawn ${amount} into Savings Account`);
-    return amount;
-
-    // Code above this line ^^^^^ ^^^^^ ^^^^^ ^^^^^ ^^^^^
-}
-
-function getTransactionHistory() {
-    // Write function description 
-    //let result = '';
-
-    // Code below this line vvvvv vvvvv vvvvv vvvvv vvvvv
-    transactionHistory.forEach((transaction, index) => {
-        console.log(`${index + 1}) ${transaction}`);
-        //result += `${index + 1}) ${transaction}\n`;
-    });
-    // Code above this line ^^^^^ ^^^^^ ^^^^^ ^^^^^ ^^^^^
-
-    //return result;
 }
 
 // Instructor's test cases
 // ===== ===== ===== ===== =====
 
-console.log(getBalance());
-// Expected Output: 5000
+const numberChecker = new NumberChecker();
 
-deposit(4000);
-console.log(getBalance());
-// Expected Output: 9000
-
-withdraw(3000);
-console.log(getBalance());
-// Expected Output: 6000
-
-//console.log(getTransactionHistory());
-getTransactionHistory();
-/* Expected Output:
-
-Transaction History
-1) Deposited 10000 into Savings Account
-2) Withdrawn 5000 from Savings Account
-3) Deposited 4000 into Savings Account
-4) Withdrawn 3000 from Savings Account
-
-*/
+console.log(numberChecker.checkOddEven(5));
+// Expected Output: Odd
+console.log(numberChecker.checkOddEven(10));
+// Expected Output: Even
+console.log(numberChecker.checkOddEven([3, 5, 7, 10]));
+// Expected Output: ['Odd', 'Odd', 'Odd', 'Even']
+console.log(numberChecker.checkOddEven('Hello'));
+// Expected Output: Invalid input
